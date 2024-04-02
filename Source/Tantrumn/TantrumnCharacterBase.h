c010193d0716bf988d56a51ba0f5b73e016c2cf7
JJisoffline <jordanli1346@gmail.com>
2024-04-02 18:41:04 +0100

    Blaargh

---
+++ Source/Tantrumn/TantrumnCharacterBase.h
@@ -0,0 +1,62 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "TantrumnCharacterBase.generated.h"
+
+UCLASS()
+class TANTRUMN_API ATantrumnCharacterBase : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	ATantrumnCharacterBase();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditAnywhere, Category = "Fall Impact")
+	float MinImpactSpeed = 100.0f;
+
+	UPROPERTY(EditAnywhere, Category = "Fall Impact")
+	float MaxImpactSpeed = 500.0f;
+
+	UPROPERTY(EditAnywhere, Category = "Fall Impact")
+	float MinStunTime = 1.0f;
+
+	UPROPERTY(EditAnywhere, Category = "Fall Impact")
+	float MaxStunTime = 1.0f;
+
+	UPROPERTY(EditAnywhere, Category = "Movement")
+	float SprintSpeed = 1200.0f;
+
+	float StunTime = 0.0f;
+	float StunBeginTimestamp = 0.0f;
+
+	bool bIsStunned = false;
+	bool bIsSprinting = false;
+
+	float MaxWalkSpeed = 0.0f;
+
+	void OnStunBegin(float StunRatio);
+	void OnStunEnd();
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	virtual void Landed(const FHitResult& Hit) override;
+
+	void RequestCrouchStart();
+	void RequestCrouchEnd();
+
+	void RequestSprintStart();
+	void RequestSprintEnd();
+};
---
+++ Source/Tantrumn/TantrumnPlayerController.h
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "TantrumnPlayerController.generated.h"
+
+class ATantrumnGameModeBase;
+
+UCLASS()
+class TANTRUMN_API ATantrumnPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+protected:
+	void SetupInputComponent() override;
+	void RequestJump();
+	void RequestStopJump();
+	void RequestMoveForward(float AxisValue);
+	void RequestMoveRight(float AxisValue);
+	void RequestLookUp(float AxisValue);
+	void RequestLookRight(float AxisValue);
+
+	void RequestCrouchStart();
+	void RequestCrouchEnd();
+
+	void RequestSprintStart();
+	void RequestSprintEnd();
+
+	UPROPERTY(EditAnywhere, Category = "Look")
+	float BaseLookUpRate = 90.0f;
+
+	UPROPERTY(EditAnywhere, Category = "Look")
+	float BaseLookRightRate = 90.0f;
+};
